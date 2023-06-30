ac48b3e1f1df79ed8c3f0382e6c767648ab03403
kanake10 <10kanake@gmail.com>
2023-06-30 21:40:08 +0300

    screen

---
+++ feature/standings/src/main/java/com/example/standings/presentation/leagues/LeagueScreen.kt
@@ -0,0 +1,78 @@
+package com.example.standings.presentation.leagues
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.example.standings.domain.model.LeaguesDomainModel
+import com.example.standings.presentation.viewmodel.LeaguesViewModel
+
+@Composable
+fun LeagueScreen(
+    navController: NavController,
+    viewModel: LeaguesViewModel = hiltViewModel()
+) {
+    val state = viewModel.leagues.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.leagues) { data ->
+                LeagueItem(
+                    league = data,
+                    onItemClick = {
+                       // navController.navigate(Screen.CoinDetailScreen.route + "/${coin.id}")
+                    }
+                )
+            }
+        }
+        if(state.error.isNotBlank()) {
+
+        }
+        if(state.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
+}
+
+@Composable
+fun LeagueItem(league: LeaguesDomainModel, onItemClick: () -> Unit) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(10.dp))
+                .height(190.dp),
+            painter = rememberImagePainter(data = league.country.flag),
+            contentDescription = " league image",
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(verticalArrangement = Arrangement.Center) {
+            Text(text = "First Text")
+            Text(text = "Second Text")
+        }
+    }
+
+}
