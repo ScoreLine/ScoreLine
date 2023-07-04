b4744617bfbcd15c5630017583384b4cb405425d
kanake10 <10kanake@gmail.com>
2023-07-04 03:34:49 +0300

    network call responses

---
+++ core-network/src/main/java/com/swayy/core_network/model/assists/Substitutes.kt
@@ -0,0 +1,7 @@
+package com.swayy.core_network.model.assists
+
+data class Substitutes(
+    val bench: Int,
+    val `in`: Int,
+    val `out`: Int
+)
---
+++ core-network/src/main/java/com/swayy/core_network/model/topscorers/Statistic.kt
@@ -0,0 +1,5 @@
+package com.swayy.core_network.model.topscorers
+
+data class Statistic(
+    val goals: com.swayy.core_network.model.topscorers.Goals
+)
